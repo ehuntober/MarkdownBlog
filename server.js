a048dd8a8dd753ecea7dfef9ab8e4da421c88cba
ehuntober <ehuntercrypto@gmail.com>
2023-06-13 18:16:32 +0000

    article field created

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ app.get('/', (req,res)=>{
     const articles = [
         {
             title: 'Test Article',
-            createdAt: Date.now(),
+            createdAt: new Date(),
             description: 'Test Description'
+        },
+        {
+            title: 'Test Article 2',
+            createdAt: new Date(),
+            description: 'Test description 2'
         }
     ]
-    res.render('index', {
+    res.render('articles/index', {
         articles : articles
     })
 })
