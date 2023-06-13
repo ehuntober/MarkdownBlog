efc9807d0dccb466d577a61006ed923b03b046bc
ehuntober <ehuntercrypto@gmail.com>
2023-06-13 17:36:23 +0000

    new update

--- routes/articles.js
+++ routes/articles.js
@@ -2,5 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/',(req,res) =>{
+    res.send('In articles')
+})
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,7 +6,17 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req,res)=>{
-    res.render('index')
+
+    const articles = [
+        {
+            title: 'Test Article',
+            createdAt: Date.now(),
+            description: 'Test Description'
+        }
+    ]
+    res.render('index', {
+        articles : articles
+    })
 })
 
 app.listen(5000)
