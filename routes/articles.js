7c28de8832df9e6f9ce0f00d37f2ecb65ffcb257
ehuntober <ehuntercrypto@gmail.com>
2023-06-14 05:13:44 +0000

    new file added

--- routes/articles.js
+++ routes/articles.js
@@ -3,15 +3,17 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new',(req,res) =>{
-    res.render('articles/new')
+    res.render('articles/new', {article: new Article})
 })
 
 router.get('/:id', (req,res) =>{
+    const article = Article.findById(req.parm)
+    res.send(req.params.id)
 
 })
 
 router.post('/', async(req,res) =>{
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
         description : req.body.description,
         markdown : req.body.markdown
@@ -22,6 +24,7 @@ router.post('/', async(req,res) =>{
         res.redirect(`/articles/${article.id}`)
     }
     catch(e){
+        console.log(e)
         res.render('/articles/new', {article: article})
     }
   
