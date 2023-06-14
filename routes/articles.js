35ed89e005ca216b0842c4ad82fe464b01861ca5
ehuntober <ehuntercrypto@gmail.com>
2023-06-14 03:52:35 +0000

    business logic done

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,21 @@
 const express = require('express')
-
+const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new',(req,res) =>{
     res.render('articles/new')
+})
+
+router.post('/', async(req,res) =>{
+    const artilce = new Article({
+        title: req.body.title,
+        description : req.bddy.description,
+        markdown : req.body.markdown
+})
+  await article.save()
+
+
+
 })
 
 
